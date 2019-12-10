eecb1f6eaf74fbcc5a5e19692d4ce85ac89de3f4
Sebastian <sebastian.arango@correo.icesi.edu.co>
2019-12-09 20:13:21 -0500

    up

--- ResearchAssistant/rawDataToFormattedData/PorCentrosDeSalud/CSVToFormattedData/src/DataFormatter.java
+++ ResearchAssistant/rawDataToFormattedData/PorCentrosDeSalud/CSVToFormattedData/src/DataFormatter.java
@@ -9,13 +9,13 @@ public class DataFormatter {
 	public static void main(String[] args) throws IOException {
 		BufferedWriter bw=new BufferedWriter(new FileWriter("rawData.txt"));
 		for (int i = 0; i < 22; i++) {
-			BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\asus\\Documents\\GitHub\\HealthCentersClusterAnalysis\\ResearchAssistant\\rawDataToFormattedData\\PorCentrosDeSalud\\CSVFILES\\"+i+".csv"));
+			BufferedReader br=new BufferedReader(new FileReader("E:\\DATA\\Repositories\\HealthCentersClusterAnalysis\\ResearchAssistant\\rawDataToFormattedData\\PorCentrosDeSalud\\CSVFILES\\"+i+".csv"));
 		
 			br.readLine();//trash
 			for (int j = 0; j < 27; j++) {
 				String[] numbers=br.readLine().split(",+");
 				for(int k=1;k<=7;k++) {
-					bw.write(numbers[k]+" ");					
+					bw.write(numbers[k]+",");					
 				}
 			}
 		
